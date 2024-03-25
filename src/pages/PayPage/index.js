ac653b3894eee3fcb11d3cdf1926e582213475b9
truotuan <truongquoctuan1409@gmail.com>
2024-03-25 21:42:37 +0700

    up pay page

--- src/pages/PayPage/index.js
+++ src/pages/PayPage/index.js
@@ -1,9 +1,45 @@
-export default function PayPage(){
+export default function PayPage() {
 
     return (
         <div className="wrapper">
             <div className="container">
-                PayPage
+                <h1 className="px-4 py-2 bg-colorPrimary text-md font-bold text-white text-center">
+                    LIÊN HỆ
+                </h1>
+                <div className="my-4">
+                    <p className="text-xl">
+                        <strong>Cách mua sim và thanh toán</strong>
+                    </p>
+                    <br />
+                    <p>
+                        <strong>Bước 1: ĐẶT SIM</strong>
+                        <br />
+                        Quý khách chọn số sim và đặt hàng trên web hoặc gọi điện đến một trong các số hotline hỗ trợ khách hàng của Sim Tuấn Hưởng.
+                    </p>
+                    <br />
+                    <p>
+                        <strong>Bước 2: XÁC NHẬN</strong>
+                        <br />
+                        Khi nhận được đơn hàng nhân viên bán hàng sẽ kiểm tra số trong kho và gọi điện lại báo cho Quý khách.
+                    </p>
+                    <br />
+                    <p>
+                        <strong>Bước 3: GIAO HÀNG</strong>
+                        <br />
+                        <strong>• Khách hàng ở Hà Nội, TP.HCM:</strong> Sim Tuấn Hưởng giao sim và thu tiền tại nhà Quý khách.
+                        <br />
+                        <strong>• Khách hàng ở tỉnh thành khác:</strong> Quý khách có thể chọn hình thức nhận sim và trả tiền hoặc chuyển tiền mua sim vào tài khoản ngân hàng của Sim Tuấn Hưởng, công ty sẽ gửi sim về nhà quý khách qua chuyển phát nhanh, thời gian 24h. Thông tin về tài khoản ngân hàng xem phía dưới đây.
+                        <br />
+                    </p>
+                    <br />
+                    <p>
+                        <strong>Lưu ý:</strong>
+                        • Sim Tuấn Hưởng Miễn Phí giao sim toàn quốc
+                        <br />
+                        • Khách hàng cần chuẩn bị trước thông tin cá nhân (trên CMND) để được vào tên chính chủ sở hữu sim.
+                    </p>
+                    <br />
+                </div>
             </div>
         </div>
     )
